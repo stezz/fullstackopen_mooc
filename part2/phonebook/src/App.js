1de644bc5f9df97a5bf2d1323cd752b833be848f
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-07-27 21:54:53 +0300

    Exercise 2.6 commit

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log("this is a new Person", {newName})
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>        
+          name: <input value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      .<ul>
+          {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
