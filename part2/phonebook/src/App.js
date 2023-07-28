a26a4dfd79e52bcfd35727ef7a6f8f32bc7c5e9a
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-07-28 16:01:26 +0300

    Exercise 2.11 commit

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Person = ({ person }) => {
   return (
@@ -62,18 +64,24 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   // declaring a stateful filterTerm to be able to pass it around
   // there may be a better way but I don't know what
   const [filterTerm, setFilterTerm] = useState('')
 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
   const addPerson = (event) => {
     // handles the addition of a new person
     event.preventDefault()
@@ -106,7 +114,6 @@ const App = () => {
       <PersonForm setNewName={setNewName} newName={newName} setNewNumber={setNewNumber} newNumber={newNumber} addPerson={addPerson}/> 
       <h2>Numbers</h2>
       <Persons persons={persons} filterTerm={filterTerm}/>
-
     </div>
   )
 }
