b8d4d11e542fd8437fb086d9367bea82829bbccb
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-08-01 16:35:39 +0300

    3.10

---
+++ part3/README.md
@@ -0,0 +1,4 @@
+# Public repository URL
+[Home](https://stezzphonebookbe.fly.dev/)
+
+[API endpoint](https://stezzphonebookbe.fly.dev/api/persons)
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -60,7 +60,8 @@ app.get("/api/info", (request, response) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
