1575015e1b72163cf9217ae0b2a21def2825736c
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-08-02 09:04:29 +0300

    Exercise 3.12

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,57 +1,69 @@
+const mongoose = require("mongoose")
 
-const mongoose = require('mongoose')
+if (
+  process.argv.length < 3 ||
+  process.argv.length == 4 ||
+  process.argv.length > 5
+) {
+  console.log(`Usage node mongo.js password [name number]`)
+  console.log(
+    "If name and number are not defined the app will return all the phonebook entries found"
+  )
+  process.exit(1)
+}
+
 const password = process.argv[2]
+const inputName = process.argv[3]
+const inputNumber = process.argv[4]
+
 const encPass = encodeURIComponent(password)
-const url =
-  `mongodb+srv://instezz:${password}@cluster0.zggnasp.mongodb.net/phApp?retryWrites=true&w=majority`
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+
+const url = `mongodb+srv://instezz:${encPass}@cluster0.zggnasp.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url).catch((err) => {
+  console.log("Error:", err)
+  mongoose.connection.close()
+  process.exit(1)
+})
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model("Person", personSchema)
 
-
-
-if (process.argv.length === 5) {
-  console.log(`adding ${process.argv[3]} to Mongo DB`)
-  const name = process.argv[3]
-  const number = process.argv[4]
-  
-  console.log(Person);
-  console.log(name, number)
+if (inputName && inputNumber) {
   const person = new Person({
-      "name": name,
-      "number": number,
-    }
-  )
-  person.save().then(result => {
-    console.log(person, "saved");
-    mongoose.connection.close()
+    name: inputName,
+    number: inputNumber,
   })
-  console.log(person)
-  let run = true
-  process.exit(1)
 
-}
-
-
-if (process.argv.length === 3) {
-  console.log('querying the database')
-  Person.find({}).then(result => {
-    console.log(result);
-    mongoose.connection.close()
-  })
-  let run = true 
-  process.exit(1)
-
-} 
-
-
-if (!run) { 
-  console.log('This program takes 3 arguments: node mongo.js {password} {"Name surname"} {phone number}')
-  process.exit(1)
+  person
+    .save()
+    .then((result) => {
+      console.log(inputName, "saved!")
+      console.log("MongoDB entry:", result)
+      mongoose.connection.close()
+    })
+    .catch((err) => {
+      console.log("Error:", err)
+      mongoose.connection.close()
+      process.exit(1)
+    })
+} else {
+  Person.find({})
+    .then((result) => {
+      console.log("phonebook")
+      result.forEach((element) => {
+        console.log("Name:", element.name, "- Number:", element.number)
+      })
+      mongoose.connection.close()
+    })
+    .catch((err) => {
+      console.log("Error:", err)
+      mongoose.connection.close()
+      process.exit(1)
+    })
 }
