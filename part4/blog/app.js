f2e85eb8ae05b0ff163f6d61f7c6f3b4481dda0b
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-08-20 11:16:41 +0300

    Refactoring index.js into app.js

---
+++ part4/blog/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { resourceUsage } = require('process')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+require('dotenv').config()
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,21 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { resourceUsage } = require('process')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-require('dotenv').config()
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
