c6b73fb32d718b06f8f13b8e18e8426ba9526df6
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-09-02 13:42:02 +0300

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,20 +1,32 @@
-const { log } = require("console")
+const { log } = require('console')
 
 const dummy = (blogs) => {
   return 1
 }
 
-
-
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
   }
-  console.log("lenght of the array is", blogs.length);
+  console.log('lenght of the array is', blogs.length)
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
-module.exports = {
-    dummy,
-    totalLikes
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    const likes = blogs.map((x) => x.likes)
+    const maxLikes = Math.max.apply(null, likes)
+    const result = (({ author, likes, title }) => ({ author, likes, title }))(
+      blogs[likes.indexOf(maxLikes)]
+    )
+    console.log(result)
+    return result
+  } else {
+    return []
   }
+}
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+}
