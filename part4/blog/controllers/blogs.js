f9a5097bb58e0e9a3f5f1cdd51ab32412b275423
Stefano Mosconi <stefano.mosconi@gmail.com>
2023-09-17 16:18:19 +0300

    4.20

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+/*
 const getTokenFrom = (request) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
@@ -10,6 +11,7 @@ const getTokenFrom = (request) => {
   }
   return null
 }
+*/
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
@@ -27,7 +29,7 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -14,6 +14,15 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
 module.exports = {
-  requestLogger
+  requestLogger,
+  tokenExtractor
 }
